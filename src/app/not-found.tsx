b597b11adcd4e9ba563ab2ea699ab621e836a1b2
pyr33x <pyrexrj@gmail.com>
2024-12-05 22:45:42 +0330

    buttons on 404

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,3 +1,7 @@
+import { GithubIcon } from "~/components/icons";
+import { CornerDownLeft } from "lucide-react";
+import { Button } from "~/components/ui";
+
 export default function NotFound() {
   return (
     <section>
@@ -7,6 +11,16 @@ export default function NotFound() {
       <p className=" text-xl md:text-2xl mt-1 font-medium text-muted-foreground text-center">
         Couldn't find the requested resource.
       </p>
+      <div className="mt-6 flex flex-row flex-wrap items-center justify-center gap-x-2">
+        <Button>
+          <GithubIcon className="mr-2 h-4 w-4" />
+          Star on Github
+        </Button>
+        <Button variant="secondary">
+          Return Home
+          <CornerDownLeft className="ml-2 h-4 w-4" />
+        </Button>
+      </div>
     </section>
   );
 }
