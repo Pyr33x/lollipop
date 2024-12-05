82d152ab4e5a469ba5278c1fa6a2f93838bf6a20
pyr33x <pyrexrj@gmail.com>
2024-12-05 11:40:45 +0330

    secure dashboard route

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,7 +1,11 @@
+import { redirect } from "next/navigation";
 import { auth } from "~/server/auth";
 
 export default async function Dashboard() {
   const session = await auth();
+  if (!session) {
+    return redirect("/");
+  }
   return (
     <>
       <h1 className="text-7xl font-bold text-white">Hello</h1>
