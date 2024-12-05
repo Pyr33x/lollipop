66b9282dd5b983d97c4ec5ae7e86b87a68b2841a
pyr33x <pyrexrj@gmail.com>
2024-12-05 11:42:22 +0330

    integrate drizzle adapter

--- src/server/auth/index.ts
+++ src/server/auth/index.ts
@@ -1,6 +1,15 @@
+import { db, accounts, sessions, users, verificationTokens } from "~/server/db";
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import GitHub from "next-auth/providers/github";
 import NextAuth from "next-auth";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: DrizzleAdapter(db, {
+    usersTable: users,
+    accountsTable: accounts,
+    sessionsTable: sessions,
+    verificationTokensTable: verificationTokens,
+  }),
   providers: [GitHub],
+  trustHost: true,
 });
