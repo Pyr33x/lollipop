65e3bdb71115ca170f51c6d37e090e4064dc8cb3
pyr33x <pyrexrj@gmail.com>
2024-12-05 20:45:22 +0330

    images in home

---
+++ src/app/(root)/_components/getImages.ts
@@ -0,0 +1,16 @@
+import { db, images } from "~/server/db";
+import { sql } from "drizzle-orm";
+
+export async function getImages({
+  searchParams,
+}: {
+  searchParams?: { asc: string };
+}) {
+  const imgs = await db
+    .select()
+    .from(images)
+    .orderBy(
+      searchParams?.asc ? sql`${images.id} ASC` : sql`${images.id} DESC`
+    );
+  return imgs;
+}
--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,7 +1,31 @@
-export default function Home() {
+import { getImages } from "~/app/(root)/_components/getImages";
+import Image from "next/image";
+import { Separator } from "~/components/ui";
+
+export const dynamic = "force-dynamic";
+
+export default async function Home() {
+  const images = await getImages({ searchParams: { asc: "id" } });
   return (
-    <>
-      <h1 className="text-7xl font-bold text-white">Hello</h1>
-    </>
+    <section>
+      <h1 className="text-2xl md:text-4xl font-bold text-foreground text-center">
+        Lolipop
+      </h1>
+      <p className=" text-xl md:text-2xl mt-1 font-medium text-muted-foreground text-center">
+        Clean Full-Stack Uploader App
+      </p>
+      <Separator className="my-6" />
+      <div className="grid grid-cols-3 gap-2">
+        {images.map((img) => (
+          <Image
+            key={img.id}
+            src={img.url ?? ""}
+            alt={img.name ?? ""}
+            width={200}
+            height={200}
+          />
+        ))}
+      </div>
+    </section>
   );
 }
